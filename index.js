16aa6653d3dcc44c639e66e626d17f00c6d326c1
Hani Ghaderi <hanighaderi@Users-MacBook-Pro.local>
2020-08-19 21:16:21 -0700

    questions for team members

--- index.js
+++ index.js
@@ -4,3 +4,87 @@ const Intern = require('./lib/Intern')
 const Engineer = require('./lib/Engineer')
 const Manager = require('./lib/Manager')
 const generatePage = require('./src/generatePage')
+
+// This is the inquirer handling the questionare for each team member, the questions are stored to be processed 
+// once all the selections a made and we no longer need to continue.
+
+const questions = {
+    managerQuestions: [
+        {
+            type: "input",
+            name: "name",
+            message: "Team's manager name:",
+            validate: validateName
+        },
+        {
+            type: "input",
+            name: "employeeID",
+            message: "Team's manager ID number:",
+            validate: validateNumber
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Team's manager email address:",
+            validate: validateEmail
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Team's manager office number:",
+            validate: validateNumber
+        },
+
+    ],
+    engineerQuestions: [
+        {
+            type: 'input',
+            name: 'name',
+            message: "Engineer's name:",
+            validate: validateName
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: "Engineer's ID number:",
+            validate: validateNumber
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Engineer's email address:",
+            validate: validateEmail
+        },
+        {
+            type: "input",
+            name: "gitHub",
+            message: "Engineer's GitHub username:",
+            validate: validateName
+        },
+    ],
+    internQuestions: [
+        {
+            type: 'input',
+            name: 'name',
+            message: "Intern's name:",
+            validate: validateName
+        },
+        {
+            type: 'input',
+            name: 'employeeID',
+            message: "Intern's ID number:",
+            validate: validateNumber
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Intern's email address:",
+            validate: validateEmail
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "Intern's current school name:",
+            validate: validateName
+        },
+    ],
